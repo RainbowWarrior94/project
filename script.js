b3da982ae94358b77ce0fc7795283aabf08de2ae
Nasta <example@gmail.com>
2023-02-09 22:50:14 +0100

    solution to the task/19 lesson

--- script.js
+++ script.js
@@ -34,19 +34,18 @@ const personalMovieDB = {
         privat: false
 } ;
 
-personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его", "");
-personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его", "");
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
 
-// const a = prompt('Один из последних просмотренных фильмов?', ''),
-//       b = prompt("На сколько оцените его", ""),
-//       c = prompt("Один из последних просмотренных фильмов?", ""),
-//       d = prompt("На сколько оцените его", ""),
 
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
+//                 МОЁ РЕШЕНИЕ
+// personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его", "");
+// personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его", "");
 // console.log(personalMovieDB);
-
-
-
